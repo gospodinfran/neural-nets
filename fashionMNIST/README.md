078e0718ca4d6bf5cdc86dc1054ba113fa132419
Fran <kezicfran@gmail.com>
2023-07-22 14:11:16 +0200

    expanded on readme

---
+++ fashionMNIST/README.md
@@ -0,0 +1,7 @@
+# Image classification on the Fashion MNIST dataset
+
+![Data](Data_labels.png)
+
+60,000 training examples and 10,000 test examples
+
+Highest achieved accuracy: 89.5%, avg loss 0.402
--- fashionMNIST/image_classification.py
+++ fashionMNIST/image_classification.py
@@ -5,10 +5,13 @@ import torchvision.models as models
 from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
+from labels import labels_map
 
-train_data = datasets.FashionMNIST("data", train=True, download="true", transform=ToTensor())
+train_data = datasets.FashionMNIST(
+    "data", train=True, download="true", transform=ToTensor())
 
-test_data = datasets.FashionMNIST("data", train=False, download="true", transform=ToTensor())
+test_data = datasets.FashionMNIST(
+    "data", train=False, download="true", transform=ToTensor())
 
 train_dataloader = DataLoader(train_data, batch_size=32)
 test_dataloader = DataLoader(test_data, batch_size=32)
@@ -30,11 +33,12 @@ class NeuralNetwork(nn.Module):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
-    
+
 
 model = NeuralNetwork()
 model.load_state_dict(torch.load("model_weights.pth"))
 model.eval()
+
 
 def train_loop(dataloader, model, loss_fn, optimizer):
     model.train()
@@ -46,11 +50,12 @@ def train_loop(dataloader, model, loss_fn, optimizer):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-    
+
         if batch % 100 == 0:
             print(
                 f"Loss: {loss.item()}, batch: {batch}"
             )
+
 
 def test_loop(dataloader, model, loss_fn):
     model.eval()
@@ -67,34 +72,36 @@ def test_loop(dataloader, model, loss_fn):
 
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(
+        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
-learning_rate = 5e-3
+learning_rate = 2e-3
 
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-epochs = 10
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train_loop(train_dataloader, model, loss_fn, optimizer)
-    test_loop(test_dataloader, model, loss_fn)
 
-# Save state
-torch.save(model.state_dict(), "model_weights.pth")
-print("DONE")
+def train_model(epochs):
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train_loop(train_dataloader, model, loss_fn, optimizer)
+        test_loop(test_dataloader, model, loss_fn)
+
+    # Save state
+    torch.save(model.state_dict(), "model_weights.pth")
+    print("DONE")
+
+# train_model(3)
 
 
-
-# figure = plt.figure(figsize=(8, 8))
-# cols, rows = 3, 3
-# for i in range(1, cols * rows + 1):
-#     sample_idx = torch.randint(size=(1, ), high=len(train_data)).item()
-#     img, label = train_data[sample_idx]
-#     figure.add_subplot(rows, cols, i)
-#     plt.title(labels_map[label])
-#     plt.axis("off")
-#     plt.imshow(img.squeeze(), cmap="gray")
-# plt.show()
-
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(size=(1, ), high=len(train_data)).item()
+    img, label = train_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
